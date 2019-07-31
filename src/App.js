a0662397d32c538575acf0ac92ce03c6205d677a
Yurii Adamenko <skyey07@gmail.com>
2019-07-31 17:07:25 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Info from "./components/info";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Info/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>weather</h2>
+                <p>узнайте погоду в вашем городе!</p>
+            </div>
+        )
+    }
+}
+
+export default Info;
