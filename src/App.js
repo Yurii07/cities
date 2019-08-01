e4a883cdc8ca2cfdde34aaaae550b2dea40e8086
Yurii Adamenko <skyey07@gmail.com>
2019-08-01 23:06:16 +0300

    add style

--- src/App.js
+++ src/App.js
@@ -53,17 +53,27 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <Info/>
-                <Form weatherMethod={this.gettingWeather}/>
-                <Weather
-                    temp={this.state.temp}
-                    city={this.state.city}
-                    country={this.state.country}
-                    sunrise={this.state.sunrise}
-                    sunset={this.state.sunset}
-                    error={this.state.error}
-                />
+            <div className="wrapper">
+                <div className="main">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-5 info">
+                                <Info/>
+                            </div>
+                            <div className="col-sm-7 form">
+                                <Form weatherMethod={this.gettingWeather}/>
+                                <Weather
+                                    temp={this.state.temp}
+                                    city={this.state.city}
+                                    country={this.state.country}
+                                    sunrise={this.state.sunrise}
+                                    sunset={this.state.sunset}
+                                    error={this.state.error}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Weather = props => (
-    <div>
+    <div className="infoWeath">
         {props.city &&
         <div>
             <p>Местоположение: {props.city}, {props.country}</p>
@@ -10,7 +10,7 @@ const Weather = props => (
             <p>Заход солнца: {props.sunset}</p>
         </div>
         }
-        <p>{props.error}</p>
+        <p className="error">{props.error}</p>
     </div>
 );
 
